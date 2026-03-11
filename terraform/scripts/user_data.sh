f0f9805bfd68d034b309b7f3c7b647290cf52243
UrielBergamini <urielbergaminii@outlook.com>
2026-03-10 23:02:05 -0300

    fix user_data branch

--- terraform/scripts/user_data.sh
+++ terraform/scripts/user_data.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-    
+
 echo "Update with latest packages"
 yum update -y
-    
+
 echo "Install Apache"
 yum install -y httpd git
-    
+
 echo "Enable Apache service to start after reboot"
-sudo systemctl enable httpd
-    
+systemctl enable httpd
+
 echo "Install application"
 cd /tmp
-git clone https://github.com/Urielbergamini/app-static-site-ec2/tree/updateapp
+git clone https://github.com/Urielbergamini/app-static-site-ec2.git
+
 cp /tmp/app-static-site-ec2/app/*.html /var/www/html/
-    
+
 echo "Start Apache service"
-service httpd restart
+systemctl start httpd
